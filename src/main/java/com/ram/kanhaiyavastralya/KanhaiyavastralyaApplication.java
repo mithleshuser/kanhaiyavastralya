43e0af0fbc7c8d49f8f658b437bcd4af7090d6dc
mithlesh1490 <mithlesh1490@gmail.com>
2020-05-07 12:22:02 +0530

    First Commit

---
+++ src/main/java/com/ram/kanhaiyavastralya/KanhaiyavastralyaApplication.java
@@ -0,0 +1,13 @@
+package com.ram.kanhaiyavastralya;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KanhaiyavastralyaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KanhaiyavastralyaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ram/kanhaiyavastralya/KanhaiyavastralyaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ram.kanhaiyavastralya;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KanhaiyavastralyaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
